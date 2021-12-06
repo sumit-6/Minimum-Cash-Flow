70783ceb3e3aa089ce0c10907f25dcbcb44620b3
sumit-6 <75926522+sumit-6@users.noreply.github.com>
2021-12-06 13:59:51 +0530

    Update Source Code.cpp

--- Source Code.cpp
+++ Source Code.cpp
@@ -14,7 +14,7 @@ using namespace std;
 int IndexOfMin(vector<int> arr);
 int IndexOfMax(vector<int> arr);
 
-void Solve(vector<int> NetPay, vector<vector<int>>& solution)
+void Solve(vector<int>& NetPay, vector<vector<int>>& solution)
 {
     int Max_Credit = IndexOfMax(NetPay);
     int Max_Debit = IndexOfMin(NetPay);
@@ -53,15 +53,15 @@ void PlotInput(vector<vector<int>>& graph)
     int N = graph.size();
     vector<vector<double>> coordinates;
     auto BackGroundColour = Scalar(255, 255, 255);
-    Mat _img(1000, 1300, CV_8UC3, BackGroundColour);
-    int boundaryRadius = 230;
+    Mat _img(1000, 1500, CV_8UC3, BackGroundColour);
+    int boundaryRadius = 200;
     int shiftX = 600, shiftY = 350;
     double TWO_PI = (double)2 * PI;
     int d = (((double)20 / TWO_PI)*atan(N)) + 5;
-    int radius = ((TWO_PI * boundaryRadius) / N) - d;
-    radius /= 4;
+    int Noderadius = ((TWO_PI * boundaryRadius) / N) - d;
+    Noderadius /= 4;
     int i = 1;
-    double fontSize = ((double)2.9 * radius) / 76, theta = 0.0;
+    double fontSize = ((double)2.9 * Noderadius) / 76, theta = 0.0;
     for (int k = 0;k < N; k++)
     {
         int x = (double)boundaryRadius*cos(theta);
@@ -73,10 +73,10 @@ void PlotInput(vector<vector<int>>& graph)
         v.push_back((double)y);
         v.push_back(theta);
         coordinates.push_back(v);
-        circle(_img, Point(x, y),radius , Scalar(0, 0, 0));
+        circle(_img, Point(x, y), Noderadius , Scalar(0, 0, 0));
         string p = to_string(i);
         i++;
-        putText(_img, p, Point(x - (radius/1.5), y + radius/4), FONT_HERSHEY_SIMPLEX, fontSize, Scalar(0, 69, 255), 1);
+        putText(_img, p, Point(x - (Noderadius/1.5), y + Noderadius/4), FONT_HERSHEY_SIMPLEX, fontSize, Scalar(0, 69, 255), 1);
         theta += TWO_PI / N;
     }
     double x0, y0, x1, y1, x2, y2, x3, y3, D;
@@ -94,11 +94,11 @@ void PlotInput(vector<vector<int>>& graph)
                 x1 = coordinates[j][0];
                 y1 = coordinates[j][1];
                 D = sqrt(((x1 - x0) * (x1 - x0)) + ((y1 - y0) * (y1 - y0)));
-                x2 = (((D- radius)*(x0)) + (radius*x1))/D;
-                y2 = (((D - radius) * (y0))+(radius * y1)) / D;
+                x2 = (((D- Noderadius)*(x0)) + (Noderadius*x1))/D;
+                y2 = (((D - Noderadius) * (y0))+(Noderadius * y1)) / D;
 
-                x3 = (((D - radius) * (x1)) + (radius * x0)) / D;
-                y3 = (((D - radius) * (y1)) + (radius * y0)) / D;
+                x3 = (((D - Noderadius) * (x1)) + (Noderadius * x0)) / D;
+                y3 = (((D - Noderadius) * (y1)) + (Noderadius * y0)) / D;
                 
                 angle = atan((y1 - y0) / (x1 - x0));
 
@@ -111,17 +111,17 @@ void PlotInput(vector<vector<int>>& graph)
                     signX = 1 - signX;
                     if (signX == 0)
                     {
-                        X0 = x3 + (radius / 3) * (cos(angle + ((double)30 * PI) / 180));
-                        X1 = x3 + (radius / 3) * cos(angle - ((double)30 * PI) / 180);
-                        Y0 = y3 + (radius / 3) * (sin(angle + ((double)30 * PI) / 180));
-                        Y1 = y3 + (radius / 3) * sin(angle - ((double)30 * PI) / 180);
+                        X0 = x3 + (Noderadius / 3) * (cos(angle + ((double)30 * PI) / 180));
+                        X1 = x3 + (Noderadius / 3) * cos(angle - ((double)30 * PI) / 180);
+                        Y0 = y3 + (Noderadius / 3) * (sin(angle + ((double)30 * PI) / 180));
+                        Y1 = y3 + (Noderadius / 3) * sin(angle - ((double)30 * PI) / 180);
                     }
                     else
                     {
-                        X0 = x3 - (radius / 3) * (cos(angle + ((double)30 * PI) / 180));
-                        X1 = x3 - (radius / 3) * cos(angle - ((double)30 * PI) / 180);
-                        Y0 = y3 - (radius / 3) * (sin(angle + ((double)30 * PI) / 180));
-                        Y1 = y3 - (radius / 3) * sin(angle - ((double)30 * PI) / 180);
+                        X0 = x3 - (Noderadius / 3) * (cos(angle + ((double)30 * PI) / 180));
+                        X1 = x3 - (Noderadius / 3) * cos(angle - ((double)30 * PI) / 180);
+                        Y0 = y3 - (Noderadius / 3) * (sin(angle + ((double)30 * PI) / 180));
+                        Y1 = y3 - (Noderadius / 3) * sin(angle - ((double)30 * PI) / 180);
                     }
                     if (S1(x0, y0, X0, Y0, R) && S1(x0, y0, X1, Y1, R))
                     {
@@ -137,13 +137,14 @@ void PlotInput(vector<vector<int>>& graph)
                 payment += to_string(graph[i][j]);
                 payment += " to ";
                 payment += to_string(j + 1);
-                arr[i] = arr[i] + (radius / 2.5);
-                putText(_img, payment, Point(x0 - (radius / 2),y0 + radius + arr[i]), FONT_HERSHEY_SIMPLEX, fontSize/4, Scalar(0, 0, 0), 1.8);
+                arr[i] = arr[i] + (Noderadius / 2.5);
+                putText(_img, payment, Point(x0 - (Noderadius / 2),y0 + Noderadius + arr[i]), FONT_HERSHEY_SIMPLEX, fontSize/4, Scalar(0, 0, 0), 1.8);
             }
         }
     }
     imshow("Image", _img);
     waitKey(0);
+    destroyWindow("Image");
 }
 
 int IndexOfMin(vector<int> arr)
